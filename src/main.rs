b26b40bb1353d3378d274c1df6cc8e47712f868d
xRetry <marcoschwarz.at@gmail.com>
2023-08-04 16:31:20 +0200

    Only react to data modification

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use notify::{Watcher, RecursiveMode, Event};
+use notify::{Watcher, RecursiveMode, Event, EventKind, event::ModifyKind};
 use regex::Regex;
 use std::path::Path;
 use anyhow::Result;
@@ -14,23 +14,30 @@ fn main() -> Result<()> {
     let mut watcher = notify::recommended_watcher(move |res: notify::Result<Event>| {
         match res {
             Ok(event) => {
-                println!("event: {:?}", &event);
-                for path_buff in &event.paths {
-                    let Some(path) = path_buff.to_str() else {
-                        continue;
-                    };
+                //println!("event: {:?}", &event);
+                match event.kind {
+                    EventKind::Modify(ModifyKind::Data(_)) => {
+                        for path_buff in &event.paths {
+                            let Some(path) = path_buff.to_str() else {
+                                continue;
+                            };
 
-                    for (reg, cb) in &reg {
-                        if reg.is_match(path) {
-                            cb(path);
+                            for (reg, cb) in &reg {
+                                if reg.is_match(path) {
+                                    cb(path);
+                                }
+                            }
                         }
-                    }
+                    },
+                    _ => {},
                 }
             }
             Err(e) => println!("watch error: {:?}", e),
         }
     })?;
 
-    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+    loop {
+        watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+    }
     Ok(())
 }
