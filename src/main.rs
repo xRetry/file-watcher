1c8c23cb04d498617edc1ce5f5fe25678dab81c7
xRetry <marcoschwarz.at@gmail.com>
2023-08-04 22:39:42 +0200

    Add generic COmmand action

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,53 @@
 use notify::{Watcher, RecursiveMode, EventKind, event::ModifyKind, RecommendedWatcher, Config};
 use regex::RegexSet;
 use std::path::Path;
-use anyhow::Result;
+use anyhow::{Result, bail};
+use std::process::Command;
 
 
 trait Action {
     fn exec_if_match(&self, path: &str);
 }
 
-struct SassAction {
+struct CommandAction<'a> {
     regex: RegexSet,
+    prog: &'a str,
+    args: Vec<&'a str>,
 }
 
-impl SassAction {
-    fn new(regex: &str) -> Result<Self> {
+impl<'a> CommandAction<'a> {
+    fn new(regex: &str, command: &'a str) -> Result<Self> {
         let regex = RegexSet::new(&[regex])?;
-        Ok(Self{ regex })
+
+        let mut cmd_split = command.split_whitespace();
+        let Some(prog) = cmd_split.next() else {
+            bail!("Invalid command!")
+        };
+        let args = cmd_split.collect();
+
+        Ok(Self{ regex, prog, args })
     }
 }
 
-impl Action for SassAction {
+impl Action for CommandAction<'_> {
     fn exec_if_match(&self, path: &str) {
         if !self.regex.is_match(path) { return }
         println!("{}", path);
+
+        let mut command = Box::new(Command::new(self.prog));
+        for arg in &self.args {
+            command.arg(arg);
+        }
+        let Ok(out) = command.output() else {
+                return
+            };
+        println!("{:?}", out);
     }
 }
 
 fn main() -> Result<()> {
     let actions = [
-        SassAction::new(r".*.scss$")?,
+        CommandAction::new(r".*.scss$", "ls -la")?,
     ];
 
     let (tx, rx) = std::sync::mpsc::channel();
