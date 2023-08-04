06731e8176667babb5650bccfb1c6635c3127fa7
xRetry <marcoschwarz.at@gmail.com>
2023-08-04 21:38:21 +0200

    Clean up file watcher

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
-use notify::{Watcher, RecursiveMode, Event, EventKind, event::ModifyKind};
-use regex::Regex;
+use notify::{Watcher, RecursiveMode, EventKind, event::ModifyKind, RecommendedWatcher, Config};
+use regex::RegexSet;
 use std::path::Path;
 use anyhow::Result;
 
 
+trait Action {
+    fn exec_if_match(&self, path: &str);
+}
+
+struct SassAction {
+    regex: RegexSet,
+}
+
+impl SassAction {
+    fn new(regex: &str) -> Result<Self> {
+        let regex = RegexSet::new(&[regex])?;
+        Ok(Self{ regex })
+    }
+}
+
+impl Action for SassAction {
+    fn exec_if_match(&self, path: &str) {
+        if !self.regex.is_match(path) { return }
+        println!("{}", path);
+    }
+}
+
 fn main() -> Result<()> {
-    let reg = [
-        (Regex::new(r".*.scss$").unwrap(), |path: &str| {
-            println!("{}", path);
-        }),
+    let actions = [
+        SassAction::new(r".*.scss$")?,
     ];
 
-    let mut watcher = notify::recommended_watcher(move |res: notify::Result<Event>| {
-        match res {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+    watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
+
+    for event in rx {
+        match event {
             Ok(event) => {
                 //println!("event: {:?}", &event);
                 match event.kind {
@@ -22,10 +47,8 @@ fn main() -> Result<()> {
                                 continue;
                             };
 
-                            for (reg, cb) in &reg {
-                                if reg.is_match(path) {
-                                    cb(path);
-                                }
+                            for action in &actions {
+                                action.exec_if_match(path);
                             }
                         }
                     },
@@ -34,10 +57,6 @@ fn main() -> Result<()> {
             }
             Err(e) => println!("watch error: {:?}", e),
         }
-    })?;
-
-    loop {
-        watcher.watch(Path::new("."), RecursiveMode::Recursive)?;
     }
     Ok(())
 }
